4bded2f7e94d71e55c30dfe60f0a5e8cc962768f
micfro <michalfrost71@gmail.com>
2023-11-29 12:14:57 +0100

    commit init

--- src/main/ShopOrderSystem.java
+++ src/main/ShopOrderSystem.java
@@ -1,8 +1,21 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ShopOrderSystem {
 
+    Scanner scanner = new Scanner(System.in);
+    private ArrayList<Product> offerList;
 
+    ShopOrderSystem() {
+        offerList = new ArrayList<>();
+        offerList.add(new Product("Table", 100));
+        offerList.add(new Product("Chair", 200));
+        offerList.add(new Product("Lamp", 300));
+        offerList.add(new Product("Window", 400));
+        offerList.add(new Product("Mirror", 500));
+    }
 
     void userInterface() {}
 }
